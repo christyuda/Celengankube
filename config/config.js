2d8d78a739ab201b8b7abaa0bfd8c9f0833d3d03
christyuda <yudacihuy09@gmail.com>
2023-10-04 00:44:12 +0700

    delete config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  secretKey: "kuncirahasia", // Ganti dengan kunci rahasia Anda
-  port: process.env.PORT || 3000, // Port yang akan digunakan oleh aplikasi
-  env: process.env.NODE_ENV || "development", // Lingkungan aplikasi (development/production)
-};
+// module.exports = {
+//   secretKey: "kuncirahasia", // Ganti dengan kunci rahasia Anda
+//   port: process.env.PORT || 3000, // Port yang akan digunakan oleh aplikasi
+//   env: process.env.NODE_ENV || "development", // Lingkungan aplikasi (development/production)
+// };
