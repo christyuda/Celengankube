b5f9293553fc70a47ede2e3f843191fac92a8d46
christyuda <yudacihuy09@gmail.com>
2023-10-09 23:06:21 +0700

    change routes

--- routes/pengeluaranRoutes.js
+++ routes/pengeluaranRoutes.js
@@ -5,10 +5,10 @@ const pengeluaranController = require("../controllers/pengeluaranController");
 // Rute untuk mendapatkan semua pengeluaran
 router.get("/", pengeluaranController.getAll);
 // Rute untuk menambahkan catatan tabungan baru
-router.post("/tambah", pengeluaranController.tambahTabungan);
+router.post("/tambah", pengeluaranController.tambahPengeluaran);
 
 // Rute untuk mendapatkan semua catatan tabungan berdasarkan ID pengguna
-router.get("/:userId", pengeluaranController.getTabungan);
+router.get("/:userId", pengeluaranController.getPengeluaran);
 
 // Rute untuk menghapus catatan tabungan berdasarkan ID
 router.delete("/:pengeluaranId", pengeluaranController.hapusPengeluaran);
--- routes/tabunganRoutes.js
+++ routes/tabunganRoutes.js
@@ -5,10 +5,10 @@ const tabunganController = require("../controllers/tabunganController");
 // Rute untuk mendapatkan semua tabungan
 router.get("/", tabunganController.getAll);
 // Rute untuk menambahkan catatan tabungan baru
-router.post("/tambah", tabunganController.tambahPengeluaran);
+router.post("/tambah", tabunganController.tambahTabungan);
 
 // Rute untuk mendapatkan semua catatan tabungan berdasarkan ID pengguna
-router.get("/:userId", tabunganController.getPengeluaran);
+router.get("/:userId", tabunganController.getTabungan);
 
 // Rute untuk menghapus catatan tabungan berdasarkan ID
 router.delete("/:tabunganId", tabunganController.hapusTabungan);
